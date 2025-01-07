9068e171ca91a68b009c686fd78f4ea464e7eeb7
M IDREES FAIZ <idreesfaiz63@gmail.com>
2025-01-07 11:11:51 +0500

    fix:footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,19 @@ import { ThemeProvider } from "./context";
 import Header from "./components/header";
 import FrontPage from "./components/frontPage";
 import Footer from "./components/Footer";
-import Satisfy from "./components/satisfy";
+// import Satisfy from "./components/satisfy";
 import Brand from "./components/brand";
 import Complain from "./components/complain";
-import Samsung from "./components/samsung";
-import Latest from "./components/Latest";
-import LatestGrid from "./globle/LatestGrid";
+// import Samsung from "./components/samsung";
+// import Latest from "./components/Latest";
+// import LatestGrid from "./globle/LatestGrid";
 
 const App = () => {
   return (
     <ThemeProvider>
       <Header />
       <FrontPage />
-     
       <Complain/>
-     
       <Brand />
       <Footer />
     </ThemeProvider>
