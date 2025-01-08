58b868625fd54c76f517e4369a0ccd110ea6d993
M IDREES FAIZ <idreesfaiz63@gmail.com>
2025-01-08 10:47:38 +0500

    errors

--- src/components/frontPage.jsx
+++ src/components/frontPage.jsx
@@ -11,7 +11,7 @@ const FrontPage = () => {
   return (
     <>
       <div
-        className={`pb-56 bg-[#82B2F0] text-black dark:bg-[#162544] dark:text-white w-full h-[140vh] flex justify-center items-end `}
+        className={`pb-56 bg-[#82B2F0] text-black dark:bg-[#162544] dark:text-white w-full h-[140vh] flex justify-center items-end`}
       >
         <Animation />
       </div>
@@ -24,54 +24,99 @@ export default FrontPage;
 
 function Animation() {
   useEffect(() => {
-    gsap.to(".card-1", {
+    gsap.to(".scroll-element", {
       width: "168px",
       height: "188",
       x: "-380px",
       y: "20px",
       marginTop: "80px",
       scrollTrigger: {
-        trigger: ".card-1",
+        trigger: ".scroll-element",
         start: "60% 80%",
-        end: "40% 65%",
+        end: "40% 55%",
         scrub: true,
-      
-        // markers: true,
+        markers: true,
       },
     });
- 
-
-    ///////////////////////////
-    gsap.to(".card2", {
+    gsap.to(".scroll-element1", {
       width: "168px",
       height: "188",
       x: "380px",
       y: "20px",
       scrollTrigger: {
-        trigger: ".card2",
+        trigger: ".scroll-element1",
         start: "60% 80%",
-        end: "40% 65%",
+        end: "40% 55%",
         scrub: true,
-      
-        // markers: true,
+        markers: true,
       },
     });
-      
-  }, []);
 
+    gsap.to(".scroll-element2", {
+      width: "168px",
+      height: "188",
+      x: "230px",
+      y: "-10px",
+
+      scrollTrigger: {
+        trigger: ".scroll-element2",
+        start: "60% 80%",
+        end: "40% 55%",
+        scrub: true,
+        markers: true,
+      },
+    });
+    gsap.to(".scroll-element3", {
+      width: "168px",
+      height: "188",
+      x: "-230px",
+      y: "-10px",
+
+      scrollTrigger: {
+        trigger: ".scroll-element3",
+        start: "60% 80%",
+        end: "40% 55%",
+        scrub: true,
+        markers: true,
+      },
+    });
+    gsap.to(".scroll-element4", {
+      width: "168px",
+      height: "188",
+      x: "70px",
+      y: "-35px",
+
+      scrollTrigger: {
+        trigger: ".scroll-element4",
+        start: "60% 80%",
+        end: "40% 55%",
+        scrub: true,
+        markers: true,
+      },
+    });
+    gsap.to(".scroll-element5", {
+      width: "168px",
+      height: "188",
+      x: "-70px",
+      y: "-30px",
+      scrollTrigger: {
+        trigger: ".scroll-element5",
+        start: "60% 80%",
+        end: "40% 55%",
+        scrub: true,
+        markers: true,
+      },
+    });
+  }, []);
 
   return (
     <>
-      <div className="absolute w-28 h-36 m-1 -rotate-[5deg] rounded-md py-3 bg-blue-600 card-1 card-overflow1">
-        <img src={watch} className="w-full h-full object-contain" alt="Watch" />
-      </div>
-      <div className="absolute w-28 h-36 rotate-[5deg] m-1 rounded-md py-3 bg-red-600 card2 ">
+      <div className="absolute w-28 h-36 m-1 rotate-[10deg] rounded-md py-3 bg-blue-600 scroll-element1">
         <img src={watch} className="w-full h-full object-contain" alt="" />
       </div>
-        {/* <div className="absolute w-28 h-36 m-1 rotate-[10deg] rounded-md py-3 bg-blue-600 scroll-element1 scroll">
-          <img src={watch} className="w-full h-full object-contain" alt="" />
-        </div>
-      
+      <div className="absolute w-28 h-36 rotate-[8deg] m-1 rounded-md py-3 bg-red-600 scroll-element2">
+        <img src={watch} className="w-full h-full object-contain" alt="" />
+      </div>
       <div className="absolute w-28 h-36 m-1 rotate-[5deg] rounded-md py-3 bg-yellow-600 scroll-element4">
         <img src={watch} className="w-full h-full object-contain" alt="" />
       </div>
@@ -80,94 +125,11 @@ function Animation() {
       </div>
       <div className="absolute w-28 h-36 m-1 -rotate-[5deg] rounded-md py-3 bg-green-600 scroll-element3">
         <img src={watch} className="w-full h-full object-contain" alt="" />
-      </div> */}
+      </div>
+      {/* The scroll-element with the animation applied */}
+      <div className="absolute w-28 h-36 m-1 -rotate-[5deg] rounded-md py-3 bg-blue-600 scroll-element">
+        <img src={watch} className="w-full h-full object-contain" alt="Watch" />
+      </div>
     </>
   );
 }
-
-
-
-
-  // gsap.to(".scroll", {
-  //   y: "350px",
-  //   marginLeft: "590px",
-  //   rotate: "0deg",
-  //   marginTop: "80px",
-  //   scrollTrigger: {
-  //     trigger: ".scroll",
-  //     start: "60% 60%",
-  //     end: "40% 35%",
-  //     scrub: true,
-  //     // markers: true,
-  //   },
-  // });
- 
-  // gsap.to(".scroll1", {
-  //   y: "350px",
-  //   marginLeft: "450px",
-  //   rotate: "0deg",
-  //   marginTop: "80px",
-  //   scrollTrigger: {
-  //     trigger: ".scroll1",
-  //     start: "60% 60%",
-  //     end: "40% 35%",
-  //     scrub: true,
-  //     // markers: true,
-  //   },
-  // });
-
-  // gsap.to(".scroll-element2", {
-  //   width: "168px",
-  //   height: "188",
-  //   x: "230px",
-  //   y: "-10px",
-
-  //   scrollTrigger: {
-  //     trigger: ".scroll-element2",
-  //     start: "60% 80%",
-  //     end: "40% 65%",
-  //     scrub: true,
-  //     // markers: true,
-  //   },
-  // });
-  // gsap.to(".scroll-element3", {
-  //   width: "168px",
-  //   height: "188",
-  //   x: "-230px",
-  //   y: "-10px",
-
-  //   scrollTrigger: {
-  //     trigger: ".scroll-element3",
-  //     start: "60% 80%",
-  //     end: "40% 65%",
-  //     scrub: true,
-  //     // markers: true,
-  //   },
-  // });
-  // gsap.to(".scroll-element4", {
-  //   width: "168px",
-  //   height: "188",
-  //   x: "70px",
-  //   y: "-35px",
-
-  //   scrollTrigger: {
-  //     trigger: ".scroll-element4",
-  //     start: "60% 80%",
-  //     end: "40% 65%",
-  //     scrub: true,
-  //     // markers: true,
-  //   },
-  // });
-  // gsap.to(".scroll-element5", {
-  //   width: "168px",
-  //   height: "188",
-  //   x: "-70px",
-  //   y: "-30px",
-  //   scrollTrigger: {
-  //     trigger: ".scroll-element5",
-  //     start: "60% 80%",
-  //     end: "40% 65%",
-  //     scrub: true,
-  //     // markers: true,
-  //   },
-  // });
